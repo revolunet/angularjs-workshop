fcbc3d66271ee5752c940f777c06a44380801c39
Julien Bouquillon <julien@bouquillon.com>
2015-02-22 00:47:15 +0100

    add Filters service for tags

--- app.js
+++ app.js
@@ -31,6 +31,73 @@ angular.module('DemoApp', [])
     }
 })
 
+.service('Filters', function() {
+    var selection = {
+        tags: []
+    };
+
+    function getItemIndex(field, value) {
+        // get the checked item position (or -1 if not selected)
+        return selection[field].indexOf(value);
+    };
+
+    return {
+        selection: selection,
+        isSelected: function(field, value) {
+            return getItemIndex(field, value) > -1;
+        },
+        toggle: function(field, value) {
+            var selectionIndex = getItemIndex(field, value);
+            if (selectionIndex > -1) {
+                // remove item
+                selection[field].splice(selectionIndex, 1);
+            } else {
+                // add item
+                selection[field].push(value);
+            }
+        }
+    }
+})
+
+.controller('FilterCtrl', function(Filters) {
+
+    this.isSelected = function(field, value) {
+        return Filters.isSelected(field, value)
+    };
+
+    this.toggle = function(field, value) {
+        Filters.toggle(field, value);
+    };
+
+})
+
+.filter('filterBooks', function(Filters) {
+    return function(array) {
+
+        var results = [];
+
+        var filterTags = Filters.selection.tags.length > 0;
+
+        angular.forEach(array, function(book) {
+            var hasTag = false,
+                hasLevel = false;
+
+            angular.forEach(book.tags, function(tag) {
+                if (Filters.selection.tags.indexOf(tag)>-1) {
+                    hasTag = true;
+                }
+            });
+
+            if ((!filterTags || hasTag)) {
+                results.push(book);
+            }
+
+        });
+
+        return results;
+    };
+})
+
 .controller('MainCtrl', function(loadBooks, uniqueArray) {
 
     function getUniqueTags() {
